fc642b439aea8be97ce57f0541101165004fe414
Jack Malone <c00236428@itcarlow.ie>
2021-10-13 16:32:36 +0100

    updated the arrival to also be dynamic

--- AI_Lab_3/ArriveBehaviour.cpp
+++ AI_Lab_3/ArriveBehaviour.cpp
@@ -22,20 +22,7 @@ void ArriveBehaviour::update(Character* t_character, float t_deltaTime)
 		if (magnitude < m_arrivalRadius) t_character->setSpeed(0);		
 		else if (magnitude > m_slowRadius) t_character->setSpeed(t_character->getMaxSpeed());
 		else t_character->setSpeed(t_character->getMaxSpeed() * (magnitude / m_slowRadius));
-		
-		sf::Vector2f velocity = steeringLinear;
-		velocity = normaliseVector(velocity);
-		velocity *= t_character->getMaxSpeed();
-		float timeToTarget = 5.5f;
-		sf::Vector2f direction = velocity - t_character->getVelocity();
-		direction /= timeToTarget;
 
-		if (vectorMagnitude(direction) > t_character->getMaxSpeed())
-		{
-			direction = normaliseVector(direction);
-			direction *= t_character->getMaxSpeed();
-		}
-
-		t_character->setVelocity(direction);
+		t_character->moveToTarget(normaliseVector(t_character->getTargetCharacter()->getPosition() - t_character->getPosition()), t_deltaTime);
 	}
 }
